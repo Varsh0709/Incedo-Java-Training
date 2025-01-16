6fe4859f8eb65adccc004034d2542dec1b894648
Varsh0709 <121662170+Varsh0709@users.noreply.github.com>
2025-01-16 17:36:03 +0530

    Add files via upload

---
+++ ArmstrongNum.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class ArmstrongNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=scan.nextInt();
+		int length=String.valueOf(num).length();
+		int sum=0;
+		int temp=num;
+		while(temp!=0) {
+			int rem=temp%10;
+			sum=sum+(int)Math.pow(rem, length);
+			temp=temp/10;
+		}
+		if(num==sum) {
+			System.out.println(num+" "+"is an armstrong number");
+		}
+		else {
+			System.out.println(num+" "+"is not an armstrong number");
+		}
+	}
+
+}
